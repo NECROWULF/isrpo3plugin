67ab304dad1b9044932dea377ca4252204c900dc
Matvey Tikhomirov <151579888+NECROWULF@users.noreply.github.com>
2024-11-28 05:48:52 -0500

    Update index.js

--- isrpo3/index.js
+++ isrpo3/index.js
@@ -1,9 +1,34 @@
 const vscode =  require('vscode');
 
-function count(list){
+function isNumber(n){
+  for(let i = 0; i < n.length; ++i){
+    if(n.substring(i,i+1) == "*" || n.substring(i,i+1) == "/"){
+      return false;
+    }
+  }
+  return true;
+}
+
+function mult(list){
+  let n = Number(list[0]);
+  for(let i = 1; i < list.length; ++i){
+    if(isNumber(list[i])){
+      n *= Number(list[i]);
+    }else{
+      n /= Number(list[i].split("/")[1]);
+    }
+  }
+  return n;
+}
+
+function sum(list){
   let n = 0;
   for(let i = 0; i < list.length; ++i){
-    n += Number(list[i]);
+    if(!isNumber(list[i])){
+      n += mult(list[i].replaceAll("/", "*1/").split("*"));
+    }else{
+      n += Number(list[i]);
+    }
   }
   return n;
 }
@@ -13,10 +38,10 @@ function calculator() {
       const selection = editor.selection;
       text = editor.document.getText(selection).replaceAll(" ","").replaceAll("-","+-").split("+");
 
-      edit.replace(selection, count(text));
+      edit.replace(selection, sum(text));
   });
 }
-
+// 1 4*9 -3*-5 5/2
 function activate(context) {
   context.subscriptions.push(calculator());
 }
